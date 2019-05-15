c2efc259349a7326055b3e3110d405e55b4f4086
Klemm S.C. (Selina) <Selina.Klemm@vanoord.com>
2019-05-15 09:28:28 +0200

    this is what you do if you want to write a lot or something like that I am not 100% sure

--- learn_git_test.py
+++ learn_git_test.py
@@ -6,6 +6,7 @@ import os
 
 def SELpercentile(SEL, percentile=5):    
     """ SELpercentile for x% highest."""
+    fff = np.percentile(SEL, 100-percentile)
 
-    return np.percentile(SEL, 100-percentile)
+    return fff
     
